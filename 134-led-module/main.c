311f9b82959e02d5cb8c48f3092e7722f8304fd3
vladislav poroskun <v82935571@gmail.com>
2026-09-18 16:18:47 +0300

    Сделана п1.3.7

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "led.h"
 #include "log.h"
+#include "device.h"
 
 const uint BUTTON_PIN = 15;
 const uint DEBOUNCE_MS = 20;
@@ -31,6 +32,10 @@ void handle_command(int command)
     else if (command == 'v')
     {
         log_version();
+    }
+    else if (command == 'i')
+    {
+    device_info();
     }
     else
     {
