b1aa41a1d09e95b6f762578950a538dd63e0622d
vladislav poroskun <v82935571@gmail.com>
2026-09-22 20:29:05 +0300

    Сделана п2.1.3

--- 211-command-usb/memory/memory.c
+++ 211-command-usb/memory/memory.c
@@ -2,10 +2,8 @@
 #include "hardware/regs/addressmap.h"
 #include "pico/stdlib.h"
 
-// ------------------------------------------------------------
 // ??????? ???????: ??????? ?????? ? ????????.
 // ????? ?????? ????? ????? &, ??? char ???? ??? 1 ????.
-// ------------------------------------------------------------
 extern char __flash_binary_start;
 extern char __flash_binary_end;
 extern char __boot2_start__;
@@ -23,18 +21,12 @@ extern char __StackTop;
 #define SRAM_SIZE_BYTES (264 * 1024)
 #define ROM_SIZE_BYTES  (16 * 1024)
 
-// ------------------------------------------------------------
-// ???? ?????? ???????: ???, ??????, ?????, ??????
-// ------------------------------------------------------------
 static void row(const char *name, uintptr_t start, uintptr_t end)
 {
     printf("%-10s 0x%08x 0x%08x %8u\n",
            name, (unsigned)start, (unsigned)end, (unsigned)(end - start));
 }
 
-// ------------------------------------------------------------
-// ??????? ?????? ???????
-// ------------------------------------------------------------
 void mem_info(void)
 {
     printf("%-10s %-10s %-10s %8s\n", "area", "start", "end", "size");
@@ -61,19 +53,28 @@ void mem_info(void)
     row("heap",  (uintptr_t)&__bss_end__,    (uintptr_t)&__HeapLimit);
     row("stack", (uintptr_t)&__StackBottom,  (uintptr_t)&__StackTop);
 
-    // ?????
+    // ??????? ????????? ?????? ??? ??????
     uintptr_t image_size = (uintptr_t)&__flash_binary_end - (uintptr_t)&__flash_binary_start;
     uintptr_t flash_free = PICO_FLASH_SIZE_BYTES - image_size;
 
-    uintptr_t ram_used = ((uintptr_t)&__data_end__ - (uintptr_t)&__data_start__)
-                       + ((uintptr_t)&__bss_end__  - (uintptr_t)&__bss_start__);
+    uintptr_t boot2_size = (uintptr_t)&__boot2_end__ - (uintptr_t)&__boot2_start__;
+    uintptr_t text_size  = (uintptr_t)&__etext      - (uintptr_t)&__boot2_end__;
+    uintptr_t data_size  = (uintptr_t)&__data_end__ - (uintptr_t)&__data_start__;
+    uintptr_t bss_size   = (uintptr_t)&__bss_end__  - (uintptr_t)&__bss_start__;
+    uintptr_t heap_size  = (uintptr_t)&__HeapLimit  - (uintptr_t)&__bss_end__;
+    uintptr_t stack_size = (uintptr_t)&__StackTop   - (uintptr_t)&__StackBottom;
 
-    uintptr_t ram_free = ((uintptr_t)&__HeapLimit    - (uintptr_t)&__bss_end__)
-                       + ((uintptr_t)&__StackTop     - (uintptr_t)&__StackBottom);
+    uintptr_t ram_used = data_size + bss_size;
 
     printf("\n");
-    printf("image size:    %8u bytes (boot2 + text + data)\n", (unsigned)image_size);
-    printf("flash free:    %8u bytes\n", (unsigned)flash_free);
-    printf("ram used:      %8u bytes (data + bss)\n", (unsigned)ram_used);
-    printf("ram free:      %8u bytes (heap + stack)\n", (unsigned)ram_free);
+    printf("total\n");
+    printf("  flash image %8u = boot2 %u + text %u + data %u\n",
+           (unsigned)image_size, (unsigned)boot2_size,
+           (unsigned)text_size, (unsigned)data_size);
+    printf("  flash free  %8u of %u\n",
+           (unsigned)flash_free, (unsigned)PICO_FLASH_SIZE_BYTES);
+    printf("  ram used    %8u = data %u + bss %u\n",
+           (unsigned)ram_used, (unsigned)data_size, (unsigned)bss_size);
+    printf("  ram free    %8u for heap and %u for stack\n",
+           (unsigned)heap_size, (unsigned)stack_size);
 }
